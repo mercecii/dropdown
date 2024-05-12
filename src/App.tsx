06858e408ae04bab819c5179a364b10530ed8424
Somnath Bakshi <somnatba@amdocs.com>
2024-05-12 17:31:24 +0530

    adding debouncing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import DropdownPage from './components/DropdownPage';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DropdownPage from "./components/DropdownPage";
+import InputDebounced from "./components/InputDebounced";
 
 function App() {
   return (
     <div className="App">
-     <DropdownPage />
+      <DropdownPage />
+      <InputDebounced />
     </div>
   );
 }
